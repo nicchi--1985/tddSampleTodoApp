b66a2e363eb4489803def3f8523f2aec414280c7
nicchi <nicchi.achan@gmail.com>
2014-12-15 00:27:31 +0900

    Initial Commit

---
+++ TodoListApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TodoListApp
+//
+//  Created by 谷 亮介 on 2014/12/15.
+//  Copyright (c) 2014年 Ryosuke Tani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ TodoListApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TodoListApp
+//
+//  Created by 谷 亮介 on 2014/12/15.
+//  Copyright (c) 2014年 Ryosuke Tani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
